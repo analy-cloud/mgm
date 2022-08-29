6f1fe3c2ee3f8e3260a0c6c7bd6f21014c4dbfda
vasu-analy <vasu.p@analy.cloud>
2022-08-29 07:02:00 +0530

    Migrated to angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { SortSettingsModel, FilterSettingsModel, Toolbar } from '@syncfusion/ej2-angular-treegrid';
+import { data } from './datasource';
+import { DataManager, WebApiAdaptor, UrlAdaptor } from '@syncfusion/ej2-data';
+import { io } from "socket.io-client";
+
+
+const SERVICE_URI = 'https://ej2services.syncfusion.com/production/web-services/api/SelfReferenceData';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  public data: Object[];
+  public sortSettings: SortSettingsModel;
+  public filterSettings: FilterSettingsModel;
+  public remoteData: DataManager;
+  public toolbar: Toolbar;
+  title = `mgm's task`;
+
+  ngOnInit(): void {
+    this.data = data;
+    this.sortSettings = {
+      // columns: [
+      //   { field: 'id', direction: 'Ascending' },
+      //   { field: 'name', direction: 'Descending' },
+      // ]
+    };
+    this.filterSettings;
+    this.remoteData = new DataManager({ url: SERVICE_URI, adaptor: new WebApiAdaptor, crossDomain: false });
+    // this.toolbar;
+
+    console.log(this.remoteData)
+  }
+}
