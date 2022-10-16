89fd227882fd00962b812163b02cfc9b32ef7a01
vasu-analy <vasu.p@analy.cloud>
2022-10-16 10:25:49 +0530

    Custom dialog added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,40 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';;
-import { FilterService, FreezeService, SortService, ToolbarService, TreeGridAllModule, TreeGridModule, VirtualScrollService } from '@syncfusion/ej2-angular-treegrid';
+import { BrowserModule } from '@angular/platform-browser';
+import { DialogModule } from '@syncfusion/ej2-angular-popups';
+import { FormsModule } from '@angular/forms';
+import {
+  EditService,
+  FilterService,
+  FreezeService,
+  SortService,
+  ToolbarService,
+  TreeGridAllModule,
+  TreeGridModule,
+  VirtualScrollService,
+} from '@syncfusion/ej2-angular-treegrid';
+
 import { enableRipple } from '@syncfusion/ej2-base';
 import { AppComponent } from './app.component';
 
 enableRipple(true);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     TreeGridAllModule,
-    TreeGridModule
+    TreeGridModule,
+    DialogModule,
+    FormsModule,
   ],
-  providers: [SortService, FilterService, ToolbarService, FreezeService, VirtualScrollService],
-  bootstrap: [AppComponent]
+  providers: [
+    SortService,
+    FilterService,
+    ToolbarService,
+    FreezeService,
+    VirtualScrollService,
+    EditService,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
